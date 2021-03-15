8bba503146796a1fd484106d2a80e5f8e543d975
Nick Fagan <nfagan@dal.ca>
2021-03-15 14:52:10 -0300

    Add compile tests

---
+++ tests/compile-tests.rs
@@ -0,0 +1,6 @@
+
+#[test]
+fn compile() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/*.rs");
+}
---
+++ tests/ui/empty.rs
@@ -0,0 +1,7 @@
+
+use vk_shader_macros::include_glsl;
+
+#[allow(dead_code)]
+const SPIR_V: &'static [u32] = include_glsl!();
+
+fn main() {}
